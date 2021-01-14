aaab1146991f5412e1a824c6e68c0bef20cb3f5f
icebox827 <icebox827@gmail.com>
2021-01-14 11:06:58 -0500

    update the greetings method

--- bin/main.rb
+++ bin/main.rb
@@ -32,28 +32,6 @@ def greetings
   puts "#{$player1} and #{$player2} let's begin"
 end
 
-
-def greetings(tokens)
-  puts 'Welcome to the Tic Tac Toe game'
-  puts 'Please enter your name player 1'
-  player1 = gets.chomp
-  puts "welcome #{player1}"
-  puts 'Please enter your name player 2'
-  player2 = gets.chomp
-  puts "welcome #{player2}"
-  # Ask Player 1 to choose a token and assign a token to player 2
-  puts "#{player1} choose your destiny token X or O"
-  player1_token = gets.chomp
-  if player1_token == tokens[0]
-    (puts "#{player1} will use #{tokens[0]} and #{player2} will use #{tokens[1]}")
-  elsif player1_token == tokens[1]
-    (puts "#{player1} will use #{tokens[1]} and #{player2} will use #{tokens[0]}")
-  else
-    puts 'Invalid token'
-  end
-  puts "Now #{player1} and #{player2} let's begin"
-end
-
 # Create the board and display it
 def board_display(board)
   puts 'Current board: '
