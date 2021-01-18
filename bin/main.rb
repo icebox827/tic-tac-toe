e8bab4e07bad0b4fdb035e296db46d6b5b2c4abc
icebox827 <icebox827@gmail.com>
2021-01-18 09:53:27 -0500

    update user entries

--- bin/main.rb
+++ bin/main.rb
@@ -8,16 +8,16 @@ tokens = %w[X O]
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # Ask player to enter their name
 puts 'Please enter your name player 1'
-$player1 = gets.chomp
+$player1 = gets.chomp.upcase
 puts "Welcome #{$player1}"
 puts 'Please enter your name player 2'
-$player2 = gets.chomp
+$player2 = gets.chomp.upcase
 puts "Welcome #{$player2}"
 
 # Ask Player 1 to choose a token and assign a token to player 2
 def user_tokens(tokens)
   puts "#{$player1} choose your destiny token X or O"
-  player1_token = gets.chomp
+  player1_token = gets.chomp.upcase
   if player1_token == tokens[0]
     (puts "#{$player1} will use #{tokens[0]} and #{$player2} will use #{tokens[1]}")
   elsif player1_token == tokens[1]
