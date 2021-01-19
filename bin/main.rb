3bc74b3721bb5d3ddb71e4e59952284b57c68f05
oxenprogrammer <emmy1000okello@gmail.com>
2021-01-19 20:04:30 +0300

    refactor(logic): add clear terminal

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,13 @@
 require_relative './../lib/games'
 puts 'Welcome to Tic Tac Toe'
 puts ' '
+def clear_terminal
+  if RUBY_PLATFORM =~ /win32|win64|\.NET|windows|cygwin|mingw32/i
+    system('cls')
+  else
+    system('clear')
+  end
+end
 tic_tac_toe = TicTacToe.new
 tic_tac_toe.display_board
 tic_tac_toe.players
