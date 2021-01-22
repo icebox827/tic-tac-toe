b35addd6a2f8ad51fcc4e748f9001f59bc310c40
oxenprogrammer <emmy1000okello@gmail.com>
2021-01-22 08:20:29 +0300

    test(player): test tie method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,9 +2,15 @@ require_relative './../lib/player'
 
 describe PlayerMove do
   player = PlayerMove.new
+  board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+  player_input = %w[X O].sample
+
   describe '#methods' do
     it 'should intantiate a class' do
       expect(player).to be_a_kind_of Object
+    end
+    it 'should return true unless a tie' do
+      expect(player.tie?(board)).to be true
     end
   end
 end
