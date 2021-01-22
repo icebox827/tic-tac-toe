449cb871e9364fa827017426e028f4e7ab4740ae
oxenprogrammer <emmy1000okello@gmail.com>
2021-01-22 08:27:13 +0300

    test(player): test valid number method true

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,7 +3,8 @@ require_relative './../lib/player'
 describe PlayerMove do
   player = PlayerMove.new
   board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-  player_input = %w[X O].sample
+  player_token = %w[X O].sample
+  player_move = [1, 2, 3, 4, 5, 6, 7, 8, 9].sample
 
   describe '#methods' do
     it 'should intantiate a class' do
@@ -11,6 +12,9 @@ describe PlayerMove do
     end
     it 'should return true unless a tie' do
       expect(player.tie?(board)).to be true
+    end
+    it 'should return true if player plays between 1 and 9 inclusive' do
+      expect(player.valid_number?(player_move)).to be true
     end
   end
 end
