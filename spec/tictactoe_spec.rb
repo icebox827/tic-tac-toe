a782224f14b106f0cf4f8c02dc327e5430e9eb9f
icebox827 <icebox827@gmail.com>
2021-01-25 13:41:30 -0500

    create first test for board

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -3,5 +3,10 @@ require './../lib/board'
 require './../lib/player'
 
 describe Board do
-  
+  context 'empty_board' do
+    it 'create a new board' do
+      board = Board.new
+      expect(board).to eq board
+    end
+  end
 end
