a8a2003f861ff3d4afd2f173517818f2c748fb42
icebox827 <icebox827@gmail.com>
2021-01-25 14:03:46 -0500

    Rename test tictactoe to board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,3 +10,11 @@ describe Board do
     end
   end
 end
+
+describe PlayerMove do
+  let(:board) { PlayerMove.new(board, player_token.to_s) }
+
+  context 'win?' do
+    let(:board) { player_token } 
+  end
+end
