9c0f5af0598ec552c59d7d8ac9f81b229bac1e99
icebox827 <icebox827@gmail.com>
2021-01-25 21:27:53 -0500

    update valide number method

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -4,14 +4,14 @@ require './lib/Validator'
 describe Validator do
   context '#valid_number?' do
     it 'Validate the number' do
-      user_input = Validator.new
-      expect(user_input).to eq user_input
+      user_input = rand(1..9)
+      expect(user_input).to be_between(1, 9).inclusive
     end
   end
 
   context '#valid_number?' do
     it 'Do not validate the number' do
-      user_input = Validator.new
+      user_input = rand(1..9)
       expect(user_input).to_not eq Validator.new
     end
   end
